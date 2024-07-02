35276145d4b6bc5fa899357756b3d1a60eaa6381
pkbtc <parasarkashyap06@gmail.com>
2024-07-02 13:58:35 +0530

    production changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import 'dotenv/config';
 const app=express();
 app.use(express.json());
-
+const port=process.env.PORT || 8080;
 let teaData=[];
 let nextId=1;
 
@@ -53,6 +54,6 @@ app.delete('/teas/:id',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is running on port 3000");
 })
